fb5b668b87e1921acdccd96b9c3c532868d3db72
kvbwork <kvbdev@yandex.ru>
2022-09-19 16:30:33 +0500

    new ShowOrderPage

---
+++ src/main/java/kvbdev/menu/impl/ShowOrderPage.java
@@ -0,0 +1,20 @@
+package kvbdev.menu.impl;
+
+import kvbdev.menu.view.Presenter;
+import kvbdev.model.Order;
+
+public class ShowOrderPage extends ActionListPage {
+    protected final Order order;
+    protected final Presenter<Order> orderPresenter;
+
+    public ShowOrderPage(Order order, Presenter<Order> orderPresenter) {
+        super("Просмотр заказа");
+        this.order = order;
+        this.orderPresenter = orderPresenter;
+    }
+
+    @Override
+    public String getView() {
+        return orderPresenter.toString(order) + "\n" + super.getView();
+    }
+}
